fd895faa9c5afa7ec76f5ec073ef67e10a720fd5
Mark Murphy <38084637+ballin2much@users.noreply.github.com>
2022-03-28 22:27:21 -0400

    Updating small AI first bug.

--- backend/staticfiles/script.js
+++ backend/staticfiles/script.js
@@ -18,6 +18,7 @@ $(document).ready(function() {
                 $(".cell").css('pointer-events', 'auto');
             })
         }
+        started = true;
     })
 
     $("#X").click(function(){
